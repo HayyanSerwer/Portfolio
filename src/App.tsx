954d9208ab3f22d911a910757ad0a647d1d1c98a
Hserwer <hayyan.serwer@gmail.com>
2026-04-04 10:43:45 +0200

    App.tsx fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home   from './pages/Home';
-import About  from './pages/About';
 import Project from './pages/Projects'
 
-function Projects() {
-  return (
-    <div className="bg-[#080808] min-h-screen flex flex-col justify-center px-[10vw]">
-      <p className="text-[10px] tracking-[4px] text-neutral-700 mb-6">PROJECTS</p>
-      <h2 className="text-4xl font-light text-white/90">Selected work.</h2>
-    </div>
-  );
-}
+
 
 function Contact() {
   return (
